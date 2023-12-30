1aec7f838bef24c2da6ce03cd02c84bbffc13e88
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2023-12-30 10:59:55 +0100

    Added iife and gameboard object and Player constructor, styled the gameboard and added container in the html

---
+++ scripts/javascript.js
@@ -0,0 +1,25 @@
+const displayBoard = (function() {
+    let partContainer = document.querySelector("#container");
+    
+    
+    for ( let i = 1; i <= 9; i++ ) {
+        let part = document.createElement("div");
+        part.classList.add("block");
+        partContainer.appendChild(part);
+    }
+})();
+
+const nothing = "vuoto";
+
+let gameboard = {
+    gameRow1: [nothing, nothing, nothing],
+    gameRow2: [nothing, nothing, nothing],
+    gameRow3: [nothing, nothing, nothing],
+};
+
+function Player(name) {
+    this.name = name;
+}
+
+let X = new Player("X");
+let O = new Player("O");
