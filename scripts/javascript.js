776cb0b5084086b43513c19c0d8b832a673aa482
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2023-12-30 11:32:57 +0100

    Added win Algorithm

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -8,6 +8,8 @@ const displayBoard = (function() {
 })();
 
 const nothing = "vuoto";
+const xMarker = "X";
+const oMarker = "O";
 
 let gameboard = {
     gameRow1: [nothing, nothing, nothing],
@@ -21,3 +23,18 @@ function Player(name) {
 
 let X = new Player("X");
 let O = new Player("O");
+
+function winAlgorithm() {
+    gameboard;
+    let placement = prompt("Scegli il posto di X o O (X1, X2, X3 / O1, O2, O3)");
+    if ( placement === "X1" ) {
+        gameboard.gameRow1[0] = xMarker;
+    }
+    if ( placement === "X2" ) {
+        gameboard.gameRow1[1] = xMarker;
+    }
+    if ( placement === "X3" ) {
+        gameboard.gameRow1[2] = xMarker;
+    }
+
+}
